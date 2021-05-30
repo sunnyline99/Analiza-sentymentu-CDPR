7e376b907530dd3d28da7634e6e5dd347d036a2f
Dawid <82048467+sunnyline99@users.noreply.github.com>
2021-05-30 14:15:46 +0200

    Update Automated.R.R

--- Automated.R.R
+++ Automated.R.R
@@ -29,16 +29,16 @@ cd_projekt_red <- searchTwitter("cd projekt red", n=2000, lang = "en")
 CD_Projekt_RED <- searchTwitter("CD Project Red", n=2000, lang = "en")
 CDPR <- searchTwitter("CDPR", n=4000, lang = "en")
 
-## Usuni璚ie retweet轑 oraz oganiczenie bazy tylko do niezb璠nych kolumn + zapis danych na dysku
+## Usunięcie retweetów oraz oganiczenie bazy tylko do niezbęnych kolumn + zapis danych na dysku
 
 tweets <- tibble::as.tibble(map_df(c(CD_Projekt_RED_Hash,Cyberpunk2077_Hash,CDPR_Hash,cd_projekt_red,CD_Projekt_RED,CDPR), as.data.frame))
 tweets_without_retweets <-tweets[tweets$isRetweet==FALSE,]
 tweets_without_retweets <- tweets_without_retweets[,c(1,3,5,11,12)]
-write.csv(tweets_without_retweets,file.path("C:\\Users\\48799\\Desktop\\RStudio\\Projekt_licencjat\\Twitter_raw_unique"), row.names = FALSE)
+write.csv(tweets_without_retweets,file.path("*ścieżka dostępu do zapisania danych (twitter_raw)*"), row.names = FALSE)
 
-## Wyb鏎 tweet轑 tylko z ostatnich 24 godzin + usuni璚ie duplikat闚
+## Wybór tweetów tylko z ostatnich 24 godzin + usunięcie duplikatów
 
-twitter_raw_unique <- read.csv("C:\\Users\\48799\\Desktop\\RStudio\\Projekt_licencjat\\Twitter_raw_unique")
+twitter_raw_unique <- read.csv("*ścieżka dostępu do zapisanych danych (twitter_raw)*")
 ostatnie_24_h <- Sys.time()-(86400)
 test_unique <- as.character.Date(twitter_raw_unique$created)>=as.character.Date(ostatnie_24_h)
 twitter_raw_unique$test <- test_unique
@@ -49,15 +49,15 @@ nowe_tweety <- nowe_tweety[!duplikaty_nowe,]
 
 tweets_new <- nowe_tweety
 
-# Za豉dowanie bazy danych ze starymi danymi i przerobienie formatu daty + usuni璚ie duplikat轑
+# Załadowanie bazy danych ze starymi danymi i przerobienie formatu daty + usunięcie duplikatów
 
-old_data <- read.csv("C:\\Users\\48799\\Desktop\\RStudio\\Projekt_licencjat\\Old_data")
+old_data <- read.csv("*ścieżka dostępu do zapisu danych (old_data)*")
 duplikaty_old <- duplicated(old_data$text)
 old_data <- old_data[!duplikaty_old,]
 row.names(old_data)<-c(1:length(old_data$text))
 old_data<-na.omit(old_data)
 
-## Po章czenie nowej i starej bazy danych + zapis bazy na dysku
+## Połączenie nowej i starej bazy danych + zapis bazy na dysku
 
 tweets_new$created <- as.character(tweets_new$created)
 dane_do_zapisu <- bind_rows(old_data,tweets_new)
@@ -69,19 +69,19 @@ dane_do_zapisu <- dane_do_zapisu[order((dane_do_zapisu$created), decreasing = T)
 row.names(dane_do_zapisu)<-c(1:length(dane_do_zapisu$text))
 dane_do_zapisu <- dane_do_zapisu[,c(1:5)]
 
-write.csv(dane_do_zapisu, file.path("C:\\Users\\48799\\Desktop\\RStudio\\Projekt_licencjat\\Old_data"), row.names = FALSE)
+write.csv(dane_do_zapisu, file.path("*ścieżka dostępu do zapisu danych (old_data)*"), row.names = FALSE)
 
-raw <- read.csv("C:\\Users\\48799\\Desktop\\RStudio\\Projekt_licencjat\\Old_data")
+raw <- read.csv("*ścieżka dostępu do zapisanych danych (old_data)*")
 duplikaty_raw <- duplicated(raw$text)
 raw <- raw[!duplikaty_raw,]
 row.names(raw)<-c(1:length(raw$text))
 raw<-na.omit(raw)
 
-write.csv(raw, file.path("C:\\Users\\48799\\Desktop\\RStudio\\Projekt_licencjat\\Old_data"), row.names = FALSE)
+write.csv(raw, file.path("*ścieżka dostępu do zapisu danych (old_data)*"), row.names = FALSE)
 
-## Wyb鏎 tylko ostatniej godziny z bazy danych (analiza kr鏒kookresowa)
+## Wybór tylko ostatniej godziny z bazy danych (analiza krótkookresowa)
 
-Dane <- read.csv("C:\\Users\\48799\\Desktop\\RStudio\\Projekt_licencjat\\Old_data")
+Dane <- read.csv("*ścieżka dostępu do zapisu danych (old_data)*")
 system_minus_godzina <- format(Sys.time()-10800, "%Y-%m-%d %H:00:00")
 system_minus_minuta <- format(Sys.time()-10800, "%Y-%m-%d %H:59:59")
 test <- as.character.Date(Dane$created)>=as.character.Date(system_minus_godzina)
@@ -92,15 +92,15 @@ Dane_godzina <- Dane[Dane$test==T,]
 Dane_godzina <- Dane_godzina[Dane_godzina$test_2==T,]
 
 
-## Czyszczenie tekstu + analiza emotikon闚
+## Czyszczenie tekstu + analiza emotikonów
 
 Dane_godzina$text <- gsub("[<>]"," ", Dane_godzina$text)
 Dane_godzina$text <- gsub("000","",Dane_godzina$text)
 Dane_godzina$text <- gsub("\\+","!",Dane_godzina$text)
 Dane_godzina$text <- gsub("U!","",Dane_godzina$text)
 
-Emotikony_db <- read.csv("C:\\Users\\48799\\Desktop\\RStudio\\Projekt_licencjat\\emoji_df.csv")
-Emotikony_sentyment <- read.csv("C:\\Users\\48799\\Desktop\\RStudio\\Projekt_licencjat\\Emoji_Sentiment_Data.csv")
+Emotikony_db <- read.csv("*ścieżka dostępu do danych emoji_df.csv*")
+Emotikony_sentyment <- read.csv("*ścieżka dostępu do danych Emoji_Sentiment_Data.csv*")
 Emotikony_sentyment$codepoints <- sprintf("%x", Emotikony_sentyment$Unicode.codepoint)
 
 Emotikony <- merge(x = Emotikony_db, y = Emotikony_sentyment, by = "codepoints", all.y = TRUE)
@@ -134,7 +134,7 @@ Emotikony_sentiment_sum <- ifelse(Emotikony_sentiment_sum >=3,3,Emotikony_sentim
 Emotikony_sentiment_sum <- ifelse(Emotikony_sentiment_sum <=-3,-3,Emotikony_sentiment_sum)
 Emotikony_sentiment_sum <- as.data.frame(Emotikony_sentiment_sum)
 
-## Czyszczenie danych ze zb璠nych znak闚
+## Czyszczenie danych ze zbędnych znaków
 
 twitterCorpus <- Corpus(VectorSource(Dane_godzina$text))
 inspect(twitterCorpus[1:10])
@@ -148,19 +148,19 @@ twitterCorpus <- tm_map(twitterCorpus, removeNumbers)
 removeURL <- function(x) gsub("http[[:alnum:]]*","",x)
 twitterCorpus <- tm_map(twitterCorpus, content_transformer(removeURL))
 
-removeURL_2 <- function(x) gsub("edua[[:alnum:]]*","",x)
-twitterCorpus <- tm_map(twitterCorpus, content_transformer(removeURL_2))
+#removeURL_2 <- function(x) gsub("edua[[:alnum:]]*","",x)
+#twitterCorpus <- tm_map(twitterCorpus, content_transformer(removeURL_2))
 
-## usuwanie znakow spoza ASCII np. cudzys這wie
+## usuwanie znakow spoza ASCII np. cudzysłowie
 
 removeNonAscii <- function(x) textclean::replace_non_ascii(x)
 twitterCorpus <- tm_map(twitterCorpus, content_transformer(removeNonAscii))
 
-## reczne usuwanie sl闚 dotyczacych emotikon闚
+## reczne usuwanie słów dotyczacych emotikonów
 
 twitterCorpus <- tm_map(twitterCorpus, removeWords,c("amp","ufef","ufeft","uufefuufefuufef","uufef","s","uffuffuufef"))
 
-## usuwanie withespace czyli podwojnych spacji i tabulator闚
+## usuwanie withespace czyli podwojnych spacji i tabulatorów
 
 twitterCorpus <- tm_map(twitterCorpus, stripWhitespace)
 twitterCorpus <- tm_map(twitterCorpus, removePunctuation)
@@ -170,7 +170,7 @@ twitterCorpus <- tm_map(twitterCorpus, removePunctuation)
 Dane_godzina[,1] <- twitterCorpus$content
 Dane_godzina <- Dane_godzina[order((Dane_godzina$created),decreasing = F),]
 
-## wa瞠nie tweet轑
+## ważenie tweetów
 
 waga_favourite_count_godzina <- ifelse(Dane_godzina$favoriteCount>=10, 2.5 ,1)
 waga_favourite_count_godzina <- as.data.frame(waga_favourite_count_godzina)
@@ -197,7 +197,7 @@ wordcloud(words = df$word, freq = sqrt(df$freq) , min.freq = 10,
           max.words=100, random.order=FALSE, 
           rot.per=0,colors=brewer.pal(8, "Dark2"), fixed.asp = F, scale = c(0.8,1))
 
-## Emocjonalno godzinna
+## Emocjonalność godzinna
 
 emotions_godzina <- get_nrc_sentiment(Dane_godzina$text)
 
@@ -274,7 +274,7 @@ legend(x=-2,y=max(wykres$suma_minuty)+1.2, legend = c("ednia senymentu"), lty
 
 ## Zapis PDF na dysku
 
-pdf("C:\\Users\\48799\\Desktop\\RStudio\\Projekt_licencjat\\Raport.pdf")
+pdf("*ścieżka dostępu do zapisu Raport.pdf*")
 
 set.seed(125) 
 
